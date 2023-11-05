f1d1c37fd9b81d6f2308773f60d6e7b1aee061ff
Boshra Asaad <boshra.galal@gmail.com>
2023-11-05 17:27:44 +1030

    Update main.js

--- main.js
+++ main.js
@@ -16,3 +16,28 @@ let blob = new Blob([JSON.stringify({
 blob = new Blob([JSON.stringify("body")], headers);
 let result = navigator.sendBeacon(url, blob);
 
+async function putSomeData() {
+    let db = await idb.open('db-name', 1, upgradeDB => upgradeDB.createObjectStore('objectStoreName', { autoIncrement: true }))
+
+    let tx = db.transaction('objectStoreName', 'readwrite')
+    let store = tx.objectStore('objectStoreName')
+
+    await store.put({ firstname: 'John', lastname: 'Doe', age: 33 })
+
+    await tx.complete
+    db.close()
+}
+
+async function getAllData() {
+    let db = await idb.open('db-name', 1)
+
+    let tx = db.transaction('objectStoreName', 'readonly')
+    let store = tx.objectStore('objectStoreName')
+
+    // add, clear, count, delete, get, getAll, getAllKeys, getKey, put
+    let allSavedItems = await store.getAll()
+
+    console.log(allSavedItems)
+
+    db.close()
+}
