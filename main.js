6ee32a437ee5cd3a94189d1bd2e7ee3e5e1f43fa
Boshra Asaad <boshra.galal@gmail.com>
2023-11-05 17:24:51 +1030

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,18 @@
 
+let formData = new FormData();
+formData.append('roomID', 'test');
+
+const headers = {
+    type: 'application/json',
+};
+let blob = new Blob([JSON.stringify({
+    data:[
+        {control:"button1"},
+        {control: "button2"},
+        {control: "button3"}
+    ]
+})], headers);
+
+blob = new Blob([JSON.stringify("body")], headers);
+let result = navigator.sendBeacon(url, blob);
+
